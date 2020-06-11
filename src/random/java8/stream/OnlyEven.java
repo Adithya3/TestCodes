43ebd7f695be85767f37c03418a55ed10b08eb55
Adithya3 <bv.adithya369@gmail.com>
2020-06-11 19:15:48 -0400

    streams added.

---
+++ src/random/java8/stream/OnlyEven.java
@@ -0,0 +1,21 @@
+package random.java8.stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OnlyEven {
+    public static List<Integer> onlyEven(List<Integer> aList) {
+        return aList.stream().filter(value -> value % 2 == 0).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> aList = Arrays.asList(1, 2, 3, 45, 53, 324, 2, 4, 43, 22);
+        aList = onlyEven(aList);
+        for (Integer value :
+                aList) {
+            System.out.println(value);
+        }
+    }
+}
---
+++ src/random/java8/stream/Stream1.java
@@ -0,0 +1,20 @@
+package random.java8.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Stream1 {
+    public static void main(String[] args) {
+//        Stream<String> aStream=Stream.generate(() -> "another thing").limit(10);
+//        aStream.forEach(s -> {
+//            System.out.println(s);
+//        });
+        String theString = Arrays.asList("a", "b", "", "dskk", "dkk", "zzz").stream().reduce("", (str1, str2) ->
+                {
+                    return str1 + str2;
+                }
+        );
+        System.out.println(theString);
+    }
+}
