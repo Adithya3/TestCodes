e45e6161daff96e75fda3fcf916cfa4a82696325
Adithya3 <bv.adithya369@gmail.com>
2020-06-29 18:39:48 -0400

    29th june

--- src/crackingCodingInterview/queuestack/SetOfStacks.java
+++ src/crackingCodingInterview/queuestack/SetOfStacks.java
@@ -1,4 +1,48 @@
 package crackingCodingInterview.queuestack;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SetOfStacks {
+    private int stackSize;
+    private int currentArray = 0;
+    private int currentIndex = -1;
+    private List<int[]> listOfStacks = new LinkedList<>();
+
+    SetOfStacks(int stackSize) {
+        this.stackSize=stackSize;
+        listOfStacks.add(new int[stackSize]);
+    }
+
+    private void addStackArray() {
+        currentArray++;
+        listOfStacks.add(currentArray, new int[stackSize]);
+    }
+
+    private void checkStackSize() {
+        if (currentIndex >= (stackSize - 1)) {
+            currentIndex = -1;
+            addStackArray();
+        } else if (currentIndex <= -1) {
+            if (currentArray != 0) {
+                currentIndex = stackSize - 1;
+                currentArray--;
+            }
+        }
+    }
+
+    public boolean push(int value) {
+        checkStackSize();
+        currentIndex++;
+        listOfStacks.get(currentArray)[currentIndex] = value;
+        return true;
+    }
+
+    public int pop() {
+        int returnValue;
+        checkStackSize();
+        returnValue = listOfStacks.get(currentArray)[currentIndex];
+        currentIndex--;
+        return returnValue;
+    }
 }
--- src/crackingCodingInterview/queuestack/stackMain.java
+++ src/crackingCodingInterview/queuestack/stackMain.java
@@ -1,10 +1,8 @@
 package crackingCodingInterview.queuestack;
 
-import crackingCodingInterview.queuestack.loweststack.LowestStack1;
-
 public class stackMain {
     public static void main(String[] args) {
-        LowestStack1 ateststack=new LowestStack1();
+        QueueByStack ateststack = new QueueByStack();
         ateststack.push(8);
         ateststack.push(3);
         ateststack.push(5);
@@ -12,8 +10,15 @@ public class stackMain {
         ateststack.push(4);
         ateststack.push(2);
         ateststack.push(56);
-        ateststack.pop();
-        ateststack.pop();
-        System.out.println(ateststack.getLow());
+        ateststack.push(66);
+        ateststack.push(81);
+        ateststack.push(6);
+        System.out.println(ateststack.pop());
+        System.out.println(ateststack.pop());
+        System.out.println(ateststack.pop());
+        System.out.println(ateststack.pop());
+        System.out.println(ateststack.pop());
+        System.out.println(ateststack.pop());
+        System.out.println(ateststack.pop());
     }
 }
